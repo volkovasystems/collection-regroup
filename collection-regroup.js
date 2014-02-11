41666b33edd0b044b6a3f11ef80b771b5868b7da
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-02-11 09:22:06 +0800

    Added collectionRegroup procedure.

---
+++ collection-regroup.js
@@ -0,0 +1,17 @@
+/*:
+	@include:
+		{
+	
+		}
+	@end-include
+*/
+collectionRegroup = function collectionRegroup( collection ){
+	/*:
+		@meta-configuration:
+			{
+				"collection:required": "Collection|Array|object"	
+			}
+		@end-meta-configuration
+	*/
+	
+};
